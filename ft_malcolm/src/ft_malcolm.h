94424422582d27e0584f349937cfb79731a942ae
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-06-18 19:54:49 -0300

    feat: Parses source and target ip addresses using inet_pton

---
+++ ft_malcolm/src/ft_malcolm.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_malcolm.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/18 19:40:17 by vgoncalv          #+#    #+#             */
+/*   Updated: 2024/06/18 19:53:36 by vgoncalv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MALCOLM_H
+# define FT_MALCOLM_H
+
+# include <stdint.h>
+
+typedef uint32_t	t_ip;
+typedef uint8_t		t_mac[6];
+
+typedef struct s_host
+{
+	t_ip	ip;
+	t_mac	mac[6];
+}	t_host;
+
+typedef struct s_malcolm
+{
+	t_host	source;
+	t_host	target;
+}	t_malcolm;
+
+#endif // !FT_MALCOLM_H
--- ft_malcolm/src/main.c
+++ ft_malcolm/src/main.c
@@ -6,14 +6,57 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 15:22:39 by vgoncalv          #+#    #+#             */
-/*   Updated: 2024/05/21 17:06:25 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2024/06/18 19:54:07 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_malcolm.h"
+#include <arpa/inet.h>
 #include <libft.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int	main(void)
+void	usage(const char *cmd)
 {
-	ft_printf("Hello, World!\n");
+	printf("Usage: %s SOURCE_IP SOURCE_MAC TARGET_IP TARGET_MAC\n", cmd);
+}
+
+int	parse_ip_addr(const char *addr, t_ip *dest)
+{
+	int	result;
+
+	if (addr == NULL || dest == NULL)
+		return (1);
+	result = inet_pton(AF_INET, addr, dest);
+	if (result <= 0)
+	{
+		dprintf(STDERR_FILENO, "Error: invalid IP address: %s\n", addr);
+		return (1);
+	}
 	return (0);
+}
+
+int	parse_arguments(t_malcolm *app, int argc, char **argv)
+{
+	if (app == NULL || argc != 5)
+		return (1);
+	if (parse_ip_addr(argv[1], &app->source.ip) != 0)
+		return (1);
+	if (parse_ip_addr(argv[3], &app->target.ip) != 0)
+		return (1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_malcolm	app;
+
+	if (argc != 5 || parse_arguments(&app, argc, argv) != 0)
+	{
+		usage(argv[0]);
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
